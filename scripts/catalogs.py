f8662cf3d16f199d7a0032acaaf1a49d654a3240
Yujing Qin <qinyj.astro@gmail.com>
2019-01-29 18:31:29 -0700

    .

--- scripts/catalogs.py
+++ scripts/catalogs.py
@@ -5,8 +5,17 @@
 '''
 
 # define catalogs to search.
-vizier_cats = ['II/246/out', 'VII/233/xsc', 'VII/237/pgc', 'V/147/sdss12',
-        'VII/259/6dfgs', 'J/ApJS/199/26/table3', 'VII/62A/mcg', 'VII/155/rc3']
+vizier_cats = [
+    'II/246/out',
+    'VII/233/xsc',
+    'II/349/ps1',
+    'VII/237/pgc',
+    'V/147/sdss12',
+    'VII/259/6dfgs',
+    # 'J/ApJS/199/26/table3',
+    # 'VII/62A/mcg',
+    # 'VII/155/rc3'
+]
 # 2MASS PSC/XSC, HyperLEDA, SDSS, 6dF, 2MRS, MCG, RC3
 
 cat_names = {
@@ -15,6 +24,7 @@ cat_names = {
     'VII/237/pgc':          'HyperLEDA',
     'V/147/sdss12':         'SDSS',
     'VII/259/6dfgs':        '6dFGS',
+    'II/349/ps1':           'PS1',
     'J/ApJS/199/26/table3': '2MRS',
     'VII/62A/mcg':          'MCG',
     'VII/155/rc3':          'RC3',
@@ -25,6 +35,7 @@ radec_cols = {
     'VII/233/xsc':          (( 2,  3), ('deg',  'deg')),
     'VII/237/pgc':          (( 1,  2), ('hour', 'deg')),
     'V/147/sdss12':         (( 0,  1), ('deg',  'deg')),
+    'II/349/ps1':           (( 0,  1), ('deg',  'deg')),
     'VII/259/6dfgs':        ((13, 14), ('hour', 'deg')),
     'J/ApJS/199/26/table3': (( 2,  3), ('deg',  'deg')),
     'VII/62A/mcg':          (( 8,  9), ('hour', 'deg')),
@@ -36,6 +47,7 @@ srcid_cols = {
     'VII/233/xsc':          0,
     'VII/237/pgc':          0,
     'V/147/sdss12':         5,
+    'II/349/ps1':           2,
     'VII/259/6dfgs':        0,
     'J/ApJS/199/26/table3': 0,
     'VII/62A/mcg':          0,
---
+++ scripts/get-image-stamps-ps1.py
@@ -0,0 +1,122 @@
+
+'''
+    Get PanSTARRS image cutout for our event candidates.
+
+    ** For compatibility issues, please run this script under python2
+'''
+
+import os, sys, time
+import warnings
+
+import logging
+import json
+from collections import OrderedDict
+
+from astropy.coordinates import SkyCoord
+
+from panstamps.downloader import downloader
+from panstamps.image import image
+
+def to_bytes(s):
+    if isinstance(s, unicode):
+        return s.encode('utf-8')
+    if isinstance(s, list):
+        return [to_bytes(item) for item in s]
+    if isinstance(s, dict):
+        return OrderedDict(
+            [(to_bytes(k), to_bytes(v)) for k, v in s.iteritems()
+        ])
+    return s
+
+if __name__ == '__main__':
+
+    logging.basicConfig()
+    log = logging.getLogger()
+
+    # read events.
+    with open('candidate-events.json', 'r') as fp:
+        cand_events = json.load(fp, object_hook=to_bytes,)
+
+    # read nearest host candidates.
+    with open('nearest-host-candidate.json', 'r') as fp:
+        nearest_hosts = json.load(fp, object_hook=to_bytes,)
+
+    if os.path.isfile('image-cutout-ps1.json'):
+        with open('image-cutout-ps1.json', 'r') as fp:
+            image_cutout = json.load(fp, object_hook=to_bytes,)
+    else:
+        image_cutout = OrderedDict()
+
+    I_counter = 0
+    for event_i, event_info_i in cand_events.iteritems():
+
+        # read RA, Dec
+        ra_i, dec_i = event_info_i['ra'], event_info_i['dec']
+        crd_i = SkyCoord(ra=ra_i, dec=dec_i, unit=('hour', 'deg'))
+
+        if crd_i.dec.deg < -35.:
+            continue # skip very low latitude sources
+
+        # having valid local images, skip
+        if (event_i in image_cutout) and (image_cutout[event_i]['ps1']):
+            continue
+
+        time.sleep(1.) # no hurry
+
+        # locate image
+        try:
+            fits_files, jpeg_files, color_files = downloader(
+                log=log,
+                settings=False,
+                downloadDirectory='./ps1-stamps/',
+                fits=False,
+                jpeg=True,
+                arcsecSize=120,
+                filterSet='gri',
+                color=True,
+                singleFilters=False,
+                ra='%f'%(crd_i.ra.deg,),
+                dec="%f"%(crd_i.dec.deg),
+                imageType="stack",
+                mjdStart=False,
+                mjdEnd=False,
+                window=False
+            ).get()
+        except:
+            continue
+            # there is an undebuggable error here.
+
+        if not color_files:
+            image_cutout[event_i] = dict(ps1=None)
+            continue # coordinates beyond survey footprint: skip.
+
+        # get image
+        myimage = image(
+            log=log,
+            settings=False,
+            imagePath=color_files[0],
+            arcsecSize=120,
+            crosshairs=True,
+            transient=False,
+            scale=True,
+            invert=False,
+            greyscale=False
+        ).get()
+
+        # and rename
+        dir_i, fname_i = os.path.split(color_files[0])
+
+        # rename.
+        new_name_i = dir_i + '/' + event_i + '-' + fname_i
+        os.rename(color_files[0], new_name_i)
+
+        # save into dict.
+        image_cutout[event_i] = dict(ps1=new_name_i)
+
+        I_counter += 1
+        if not I_counter % 7:
+            with open('image-cutout-ps1.json', 'w') as fp:
+                json.dump(image_cutout, fp)
+
+    with open('image-cutout-ps1.json', 'w') as fp:
+        json.dump(image_cutout, fp)
